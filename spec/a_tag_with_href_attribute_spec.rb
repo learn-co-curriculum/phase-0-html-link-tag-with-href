9f99a897132ec0033e5417f4a533783e3c92d418
Gabe Jackson <gj@mail.co.de>
2017-11-02 17:06:13 -0400

    Extract Nokogiri calls into spec_helper.rb so as to avoid confusing students

--- spec/a_tag_with_href_attribute_spec.rb
+++ spec/a_tag_with_href_attribute_spec.rb
@@ -1,34 +1,20 @@
 RSpec.describe "An <a> tag with an 'href' attribute" do
 
   it 'contains an <a> tag' do
-    html = Nokogiri::HTML(File.read("./index.html")) do |config| 
-      config.strict.dtdload.dtdvalid.noblanks
-    end
-
-    a_tag = html.search("a").first
+    a_tag = parsed_html.search('a').first
 
     expect(a_tag).to_not be_nil
   end
 
-  it 'has the inner text of Flatiron School' do
-    contents = File.read("./index.html")
-    html = Nokogiri::HTML(contents) do |config| 
-      config.strict.dtdload.dtdvalid.noblanks
-    end
+  it "has the inner text of 'Flatiron School'" do
+    a_tag = parsed_html.search('a').first
 
-    a_tag = html.search("a").first
-
-    expect(a_tag.text).to eq("Flatiron School")
+    expect(a_tag.text).to eq('Flatiron School')
   end
 
-  it 'has the href attribute of https://flatironschool.com' do
-    contents = File.read("./index.html")
-    html = Nokogiri::HTML(contents) do |config| 
-      config.strict.dtdload.dtdvalid.noblanks
-    end
+  it "has the href attribute of 'https://flatironschool.com'" do
+    a_tag = parsed_html.search('a').first
 
-    a_tag = html.search("a").first
-
-    expect(a_tag.attr("href")).to eq("https://flatironschool.com")
+    expect(a_tag.attr('href')).to eq('https://flatironschool.com')
   end
 end
